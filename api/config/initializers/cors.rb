deca4fb9eeb9b4b711801dc24a6253a22c10eff9
Aravindhan <aravikmu@gmail.com>
2022-11-22 22:41:40 +0530

    🚀 Initial Set up for API 🚀

---
+++ api/config/initializers/cors.rb
@@ -0,0 +1,20 @@
+# Be sure to restart your server when you modify this file.
+
+# Avoid CORS issues when API is called from the frontend app.
+# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
+
+# Read more: https://github.com/cyu/rack-cors
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    if Rails.env.development?
+      origins 'localhost:3000'
+    else
+      origins '*' # need to setup for Production
+    end
+
+    resource '*',
+      headers: :any,
+      methods: %i[get post put patch delete options head]
+  end
+end
