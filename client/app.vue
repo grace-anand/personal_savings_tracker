a81a335e3d1cb5a1ebc19db2e2d324e92dfa3386
prasanth310 <prasanth.sathya103@gmail.com>
2022-12-04 01:30:01 +0530

    Installed vee validate with zod

--- client/app.vue
+++ client/app.vue
@@ -1,3 +1,21 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <Form class="flex justify-center items-center h-screen">
+    <Field
+      class="border-black border-solid border"
+      name="email"
+      type="email"
+      placeholder="Enter email"
+      :rules="fieldSchema"
+    />
+    <ErrorMessage name="email" />
+  </Form>
 </template>
+
+<script setup>
+import { Field, Form, ErrorMessage } from "vee-validate";
+import { toFieldValidator } from "@vee-validate/zod";
+import * as zod from "zod";
+const fieldSchema = toFieldValidator(
+  zod.string().nonempty("Field is required").email("Must be a valid email")
+);
+</script>
